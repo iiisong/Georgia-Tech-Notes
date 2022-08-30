402d3bd84af0be8be9aeee8e6755be3da477e7ec
iiisong <isaacsong03@gmail.com>
2022-08-29 20:45:35 -0400

    CS 1331 PE0

---
+++ CS 1331/Assignments/PE0/Test.java
@@ -0,0 +1,8 @@
+// class header
+public class Test {
+    // main
+    public static void main(String[] args) {
+        // print "Hello World"!
+        System.out.println("Hello World!");
+    }
+}
